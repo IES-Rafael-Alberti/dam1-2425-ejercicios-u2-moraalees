81d4745b07a4652e49e372ee96968f9d4f6e1f44
cmorcan602 <mccristian@iesrafaelalberti.es>
2024-11-04 18:52:57 +0100

    documentacion logaritmo burbuja

--- src/depuracion/algoritmo_burbuja.py
+++ src/depuracion/algoritmo_burbuja.py
@@ -1,4 +1,17 @@
 def algoritmo(arr):
+    """
+    Ordena una lista de números utilizando el algoritmo de ordenamiento de burbuja.
+    
+    Este método recorre repetidamente la lista, comparando elementos adyacentes y
+    cambiándolos de posición si están en el orden incorrecto. Este proceso se repite 
+    hasta que la lista esté ordenada.
+
+    Parameters:
+    arr (list): Lista de números a ordenar.
+
+    Returns:
+    list: La lista ordenada en orden ascendente.
+    """
     n = len(arr)
     for i in range(n - 1):
         for j in range(n - 1 - i):
@@ -8,11 +21,16 @@ def algoritmo(arr):
 
 
 def main():
+    """
+    Función principal que ejecuta el programa de ordenamiento de una lista.
+    
+    Define una lista de números, la ordena utilizando el algoritmo de burbuja y
+    muestra el resultado en la consola.
+    """
     a = [8, 3, 1, 19, 14]
     sorted_a = algoritmo(a)
     
     print(f"Lista ordenada: {sorted_a}")
-    
     
     
 if __name__ == "__main__":
