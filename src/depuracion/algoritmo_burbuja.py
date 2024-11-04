a460a400246412e0c8b7e3ae49384b5ae40eaf42
cmorcan602 <mccristian@iesrafaelalberti.es>
2024-11-04 18:46:22 +0100

    fin algoritmo burbuja

--- src/depuracion/algoritmo_burbuja.py
+++ src/depuracion/algoritmo_burbuja.py
@@ -1,5 +1,17 @@
+def algoritmo(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]: 
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
 def main():
     a = [8, 3, 1, 19, 14]
+    sorted_a = algoritmo(a)
+    
+    print(f"Lista ordenada: {sorted_a}")
     
     
     
